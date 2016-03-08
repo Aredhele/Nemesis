9de952e1abd1577f2cd359643d532892f56ee2d9
Gaan25 <mathieu.pourchot@gmail.com>
2016-03-08 20:02:14 +0100

    images persos sur la table

--- client/client/src/Game.cpp
+++ client/client/src/Game.cpp
@@ -7,41 +7,42 @@ Game::Game(bool debug, ManagerGroup * ptr_managerGroup):
         BasicInterface(debug, ptr_managerGroup)
 
 {
-	//Remington
-    ptr_remingtonInGame = ptr_managerGroup->ptr_textureManager->getTexture("remingtonInGame");
-    ptr_remingtonInGame->setSmooth(true);
-    m_panelRemingtonInGame.create("remingtonInGame",765,170,ptr_remingtonInGame);
-    getContentPane()->addComponent(&m_panelRemingtonInGame);
-
-	//Eldora
-    ptr_eldoraInGame = ptr_managerGroup->ptr_textureManager->getTexture("eldoraInGame");
-    ptr_eldoraInGame->setSmooth(true);
-    m_panelEldoraInGame.create("eldoraInGame",780,292,ptr_eldoraInGame);
-    getContentPane()->addComponent(&m_panelEldoraInGame);
-
-    //Numero 8
-    ptr_numero8InGame = ptr_managerGroup->ptr_textureManager->getTexture("numero8InGame");
-    ptr_numero8InGame->setSmooth(true);
-    m_panelNumero8InGame.create("numero8InGame",100,170,ptr_numero8InGame);
-    getContentPane()->addComponent(&m_panelNumero8InGame);
-
     //MDJ
     ptr_mdjInGame = ptr_managerGroup->ptr_textureManager->getTexture("mdjInGame");
     ptr_mdjInGame->setSmooth(true);
     m_panelMdjInGame.create("mdjInGame",430,50,ptr_mdjInGame);
     getContentPane()->addComponent(&m_panelMdjInGame);
 
-    //Tristan
-    ptr_tristanInGame = ptr_managerGroup->ptr_textureManager->getTexture("tristanInGame");
-    ptr_tristanInGame->setSmooth(true);
-    m_panelTristanInGame.create("tristanInGame",95,312,ptr_tristanInGame);
-    getContentPane()->addComponent(&m_panelTristanInGame);
-
     //Table
     ptr_tableInGame = ptr_managerGroup->ptr_textureManager->getTexture("tableInGame");
     ptr_tableInGame->setSmooth(true);
     m_panelTableInGame.create("tableInGame",190,170,ptr_tableInGame);
     getContentPane()->addComponent(&m_panelTableInGame);
+
+	//Remington
+    ptr_remingtonInGame = ptr_managerGroup->ptr_textureManager->getTexture("remingtonInGame");
+    ptr_remingtonInGame->setSmooth(true);
+    m_panelRemingtonInGame.create("remingtonInGame",700,170,ptr_remingtonInGame);
+    getContentPane()->addComponent(&m_panelRemingtonInGame);
+
+	//Eldora
+    ptr_eldoraInGame = ptr_managerGroup->ptr_textureManager->getTexture("eldoraInGame");
+    ptr_eldoraInGame->setSmooth(true);
+    m_panelEldoraInGame.create("eldoraInGame",710,270,ptr_eldoraInGame);
+    getContentPane()->addComponent(&m_panelEldoraInGame);
+
+    //Numero 8
+    ptr_numero8InGame = ptr_managerGroup->ptr_textureManager->getTexture("numero8InGame");
+    ptr_numero8InGame->setSmooth(true);
+    m_panelNumero8InGame.create("numero8InGame",250,170,ptr_numero8InGame);
+    getContentPane()->addComponent(&m_panelNumero8InGame);
+
+    //Tristan
+    ptr_tristanInGame = ptr_managerGroup->ptr_textureManager->getTexture("tristanInGame");
+    ptr_tristanInGame->setSmooth(true);
+    m_panelTristanInGame.create("tristanInGame",250,280,ptr_tristanInGame);
+    getContentPane()->addComponent(&m_panelTristanInGame);
+
 }
 
 /*!
