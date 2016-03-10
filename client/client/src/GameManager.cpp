8800548b023a8af1f51c9ba1c389f7631fd687f3
pachobo16 <pachobo16@hotmail.fr>
2016-03-10 01:45:08 +0100

    description des personnages

--- client/client/src/GameManager.cpp
+++ client/client/src/GameManager.cpp
@@ -45,29 +45,39 @@ Player *  GameManager::getPlayer(){
 void GameManager::initCharacters(){
     m_characterList[0]->setName(L"Numero 8");
     m_characterList[0]->setId("numero8");
-    m_characterList[0]->setDescription(L"");
+    m_characterList[0]->setDescription(L"Salut à toi !\n J’ai pour seul nom le chiffre 8.\nIl me vient de la prison dans laquelle je suis née.\nJ’y ai été entrainé pour devenir un guerrier puissant. Rien ne m’arrête !\nJe fonce dans la bataille tête baissée et je n’ai jamais été vaincu. ");
+    m_characterList[0]->setCaracteristics(50,3000,30);
+    m_characterList[0]->setAbility(0,"Toto","toto");
 
     m_characterList[1]->setName(L"Remington");
     m_characterList[1]->setId("remington");
-    m_characterList[1]->setDescription(L"");
+    m_characterList[1]->setDescription(L"… Je suis né de la nuit.\nRelevé parmi les morts, j’aime les recoins sombres et discrets pour frapper\nmes ennemis dans le dos.");
+    m_characterList[1]->setCaracteristics(500,4000,35);
+    m_characterList[1]->setAbility(10, "Toto", "toto");
 
     m_characterList[2]->setName(L"Eldora Dragnir");
     m_characterList[2]->setId("eldora");
-    m_characterList[2]->setDescription(L"");
+    m_characterList[2]->setDescription(L"Bonjour à vous !\nJe suis une elfe de la lumière.\nElevée dans le royaume de la Lorian, mes compétences en soin sont inégalées\ndans toutes les terres du nord.");
+    m_characterList[2]->setCaracteristics(250,7000,40);
+    m_characterList[2]->setAbility(20, "Toto", "toto");
 
     m_characterList[3]->setName(L"Tristan de la Mortifère");
     m_characterList[3]->setId("tristan");
-    m_characterList[3]->setDescription(L"");
+    m_characterList[3]->setDescription(L"Je me nomme Tristan de la Mortifère.\nJe suis originaire du Royaume du Sud.\nMon père, le duc de Mortifère, m’a entrainé depuis mon enfance à tous\nles styles de combats.\nMa polyvalence fait de moi un chevalier redoutable.");
+    m_characterList[3]->setCaracteristics(100,15000,50);
+    m_characterList[3]->setAbility(30, "Toto", "toto");
 
     m_characterList[4]->setName(L"Maitre du jeu");
     m_characterList[4]->setId("mdj");
-    m_characterList[4]->setDescription(L"");
+    m_characterList[4]->setDescription(L"Bienvenue voyageur ! \nJe suis le maître du jeu. \nVotre destin reposera entre mes mains…");
+    m_characterList[4]->setCaracteristics(0,0,0);
+    m_characterList[4]->setAbility(40, "Toto", "toto");
 
-    for (int i = 0; i < 5; i++){
+    /*for (int i = 0; i < 5; i++){
         m_characterList[i]->setDescription(L"Bonjour ! \nJe m\'appelle "
                                         + m_characterList[i]->getName()
                                            + L" !");
-    }
+    }*/
 }
 
 Character* GameManager::getCharacterById(std::string id){
